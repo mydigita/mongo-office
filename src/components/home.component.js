e5e6fd0fa57776277f5a0f226ed07322c86a3040
thetradecoder <thetradecoder@gmail.com>
2020-11-03 13:00:09 +0600

    local update

--- src/components/home.component.js
+++ src/components/home.component.js
@@ -1,12 +1,20 @@
 import React from "react";
-
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.js";
+import "jquery/dist/jquery.js";
+import "../App.css";
 
 export default function Home(){
 
     return (
         <div>
-         <h1>Mongo Office</h1>
-         <p>by: theTradeCoder</p>
+            <div className="container">
+                <h1>Mongo Office</h1>
+                <p>by: theTradeCoder</p>
+            </div>
+            <div>
+
+            </div>
         </div>
     );
 
