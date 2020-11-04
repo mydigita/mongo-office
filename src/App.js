8761f617e95b46e92e8049cd09831dd82aaf0098
thetradecoder <thetradecoder@gmail.com>
2020-11-04 15:03:21 +0600

    local update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
 import SideNav from "./components/side-nav.component.js";
+import TaskManager from "./components/task-manager.component.js";
 
 function App() {
   return (
     <Router className="App">
       <Route path="/" component={SideNav}/>
+      <Route path="/mongo-office/task-manager" component={TaskManager}/>
     </Router>
   );
 }
--- src/components/task-manager.component.js
+++ src/components/task-manager.component.js
@@ -1,12 +1,45 @@
 import React from 'react';
 import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.js";
+import "jquery/dist/jquery.js";
 import "../App.css";
 
 
 export default function TaskManager(){
     return(
-        <div>
-            <h1>Task Manager</h1>        
+        <div className="body-part">
+            <div>
+                <h1 className="text-center">Task Manager</h1>
+                <hr/>
+            </div>
+            <div>
+            <ul class="nav nav-tabs pt-2">
+                <li class="nav-item">
+                    <a class="nav-link active primary" data-toggle="tab" href="#view">View Task</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link warning" data-toggle="tab" href="#add">Add Task</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link danger" data-toggle="tab" href="#edit">Edit Task</a>
+                </li>
+            </ul>
+
+                <div className="tab-content">
+                    <div id="view" className="tab-pane fade in active">
+                        <h3>Task List</h3>
+                        <p>Task</p>
+                    </div>
+                    <div id="add" className="tab-pane fade">
+                        <h3>Add New Task</h3>
+                        <p>New task</p>
+                    </div>
+                    <div id="edit" className="tab-pane fade">
+                        <h3>Edit Task</h3>
+                        <p>Edit task</p>
+                    </div>                   
+                </div>
+            </div>
         </div>
     );
 }
