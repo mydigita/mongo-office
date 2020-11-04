e2f1ec9a5f6681c10fdb3f3f7ea114e6bd2dc708
thetradecoder <thetradecoder@gmail.com>
2020-11-04 10:01:56 +0600

    local update

--- src/components/side-nav.component.js
+++ src/components/side-nav.component.js
@@ -16,20 +16,20 @@ export default function SideNav(){
                 </div>
                 <div>
                     <a href="/task-manager/" className="form-control nav-link">Task Manager</a>
-                    <a href="/event-manager/" className="form-control nav-link">Event Manager</a>
+                    <a href="/event-manager/" className="form-control nav-link">Event Manager</a>                    
+                    <a href="/file-tracker/" className="form-control nav-link">File Tracker</a>
+                    <a href="/duty-roster/" className="form-control nav-link">Duty Roster</a>
+                    <a href="/vehicle-records/" className="form-control nav-link">Vehicle Records</a> 
+                    <a href="/travel-records/" className="form-control nav-link">Travel Records</a>
+                    <a href="/meeting-records/" className="form-control nav-link">Meeting Records</a>
+                    <a href="/medical-records/" className="form-control nav-link">Medical Records</a>                    
+                    <a href="/plan-master/" className="form-control nav-link">Plan Master</a>
+                    <a href="/project-manager/" className="form-control nav-link">Project Manager</a>        
                     <a href="/business-contacts/" className="form-control nav-link">Business Contacts</a>
                     <a href="/employee-contacts/" className="form-control nav-link">Employee Contacts</a>
                     <a href="/airline-contacts/" className="form-control nav-link">Airline Contacts</a>
                     <a href="/association-contacts/" className="form-control nav-link">Association Contacts</a>
                     <a href="/embassy-contacts/" className="form-control nav-link">Embassy Contacts</a>
-                    <a href="/file-tracker/" className="form-control nav-link">File Tracker</a>
-                    <a href="/travel-records/" className="form-control nav-link">Travel Records</a>
-                    <a href="/medical-records/" className="form-control nav-link">Medical Records</a>
-                    <a href="/meeting-records/" className="form-control nav-link">Meeting Records</a>
-                    <a href="/plan-master/" className="form-control nav-link">Plan Master</a>
-                    <a href="/project-manager/" className="form-control nav-link">Project Manager</a>
-                    <a href="/vehicle-records/" className="form-control nav-link">Vehicle Records</a>            
-                    <a href="/duty-roster/" className="form-control nav-link">Duty Roster</a>
                 </div>
             </div>
         </div>
