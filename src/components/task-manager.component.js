c6570707e55e5084322cee4da8111485ae95f4ea
thetradecoder <thetradecoder@gmail.com>
2020-11-04 14:24:01 +0600

    creating task manager

---
+++ src/components/task-manager.component.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "../App.css";
+
+
+export default function TaskManager(){
+    return(
+        <div>
+            <h1>Task Manager</h1>        
+        </div>
+    );
+}
