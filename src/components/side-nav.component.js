25cc19be583b09f77290fbf28c50f96c0cc10692
thetradecoder <thetradecoder@gmail.com>
2020-11-04 10:24:07 +0600

    local update

--- src/components/side-nav.component.js
+++ src/components/side-nav.component.js
@@ -15,23 +15,23 @@ export default function SideNav(){
                     <p>by: theTradeCoder</p>
                 </div>
                 <div>
-                    <a href="/task-manager/" className="form-control nav-link">Task Manager</a>
-                    <a href="/event-manager/" className="form-control nav-link">Event Manager</a>                    
-                    <a href="/file-tracker/" className="form-control nav-link">File Tracker</a>
-                    <a href="/duty-roster/" className="form-control nav-link">Duty Roster</a>
-                    <a href="/cash-register/" className="form-control nav-link">Cash Register</a>                   
-                    <a href="/vehicle-records/" className="form-control nav-link">Vehicle Records</a> 
-                    <a href="/travel-records/" className="form-control nav-link">Travel Records</a>
-                    <a href="/meeting-records/" className="form-control nav-link">Meeting Records</a>
-                    <a href="/medical-records/" className="form-control nav-link">Medical Records</a>                    
-                    <a href="/plan-master/" className="form-control nav-link">Plan Master</a>
-                    <a href="/project-manager/" className="form-control nav-link">Project Manager</a>        
-                    <a href="/business-contacts/" className="form-control nav-link">Business Contacts</a>
-                    <a href="/employee-contacts/" className="form-control nav-link">Employee Contacts</a>
-                    <a href="/airline-contacts/" className="form-control nav-link">Airline Contacts</a>
-                    <a href="/association-contacts/" className="form-control nav-link">Association Contacts</a>
-                    <a href="/embassy-contacts/" className="form-control nav-link">Embassy Contacts</a>
-                    <a href="/visa-centers/" className="form-control nav-link">Visa Centers</a>
+                    <a href="/mongo-office/task-manager/" className="form-control nav-link">Task Manager</a>
+                    <a href="/mongo-office/event-manager/" className="form-control nav-link">Event Manager</a>                    
+                    <a href="/mongo-office/file-tracker/" className="form-control nav-link">File Tracker</a>
+                    <a href="/mongo-office/duty-roster/" className="form-control nav-link">Duty Roster</a>
+                    <a href="/mongo-office/cash-register/" className="form-control nav-link">Cash Register</a>                   
+                    <a href="/mongo-office/vehicle-records/" className="form-control nav-link">Vehicle Records</a> 
+                    <a href="/mongo-office/travel-records/" className="form-control nav-link">Travel Records</a>
+                    <a href="/mongo-office/meeting-records/" className="form-control nav-link">Meeting Records</a>
+                    <a href="/mongo-office/medical-records/" className="form-control nav-link">Medical Records</a>                    
+                    <a href="/mongo-office/plan-master/" className="form-control nav-link">Plan Master</a>
+                    <a href="/mongo-office/project-manager/" className="form-control nav-link">Project Manager</a>        
+                    <a href="/mongo-office/business-contacts/" className="form-control nav-link">Business Contacts</a>
+                    <a href="/mongo-office/employee-contacts/" className="form-control nav-link">Employee Contacts</a>
+                    <a href="/mongo-office/airline-contacts/" className="form-control nav-link">Airline Contacts</a>
+                    <a href="/mongo-office/association-contacts/" className="form-control nav-link">Association Contacts</a>
+                    <a href="/mongo-office/embassy-contacts/" className="form-control nav-link">Embassy Contacts</a>
+                    <a href="/mongo-office/visa-centers/" className="form-control nav-link">Visa Centers</a>
                 </div>
             </div>
         </div>
