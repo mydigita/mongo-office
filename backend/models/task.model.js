029b881b545dda73074ffc713f56a1655bdbc62f
thetradecoder <thetradecoder@gmail.com>
2020-11-04 22:49:24 +0600

    created task model with mongoose shcema

---
+++ backend/models/task.model.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const TaskSchema = new Schema({
+    username:{type:String},
+    title:{type:String},
+    details:{type:String},
+    assigned_to:{type:String},
+    created_by:username,
+    date_created:{type:Date},
+    deadline:{type:Date},
+    status:{type:String},
+    ip:[String],
+    updated_on:[String],
+    mac:[String],
+    update_count:{type:Number},
+    deleted:{type:String},
+    closed:{type:String},
+    open:{type:String}
+}, {timestamps:true});
+
+const Task = mongoose.model('Task', TaskSchema);
+module.exports = Task;
