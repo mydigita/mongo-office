0490dc98867bda8a0632d8badbfaa853b29bd0ad
thetradecoder <thetradecoder@gmail.com>
2020-11-04 15:51:07 +0600

    local update

--- src/components/side-nav.component.js
+++ src/components/side-nav.component.js
@@ -1,4 +1,5 @@
 import React from "react";
+import {Link} from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.js";
 import "jquery/dist/jquery.js";
@@ -15,23 +16,23 @@ export default function SideNav(){
                     <p>by: theTradeCoder</p>
                 </div>
                 <div>
-                    <a href="/mongo-office/task-manager/" className="form-control nav-link">Task Manager</a>
-                    <a href="/mongo-office/event-manager/" className="form-control nav-link">Event Manager</a>                    
-                    <a href="/mongo-office/file-tracker/" className="form-control nav-link">File Tracker</a>
-                    <a href="/mongo-office/duty-roster/" className="form-control nav-link">Duty Roster</a>
-                    <a href="/mongo-office/cash-register/" className="form-control nav-link">Cash Register</a>                   
-                    <a href="/mongo-office/vehicle-records/" className="form-control nav-link">Vehicle Records</a> 
-                    <a href="/mongo-office/travel-records/" className="form-control nav-link">Travel Records</a>
-                    <a href="/mongo-office/meeting-records/" className="form-control nav-link">Meeting Records</a>
-                    <a href="/mongo-office/medical-records/" className="form-control nav-link">Medical Records</a>                    
-                    <a href="/mongo-office/plan-master/" className="form-control nav-link">Plan Master</a>
-                    <a href="/mongo-office/project-manager/" className="form-control nav-link">Project Manager</a>        
-                    <a href="/mongo-office/business-contacts/" className="form-control nav-link">Business Contacts</a>
-                    <a href="/mongo-office/employee-contacts/" className="form-control nav-link">Employee Contacts</a>
-                    <a href="/mongo-office/airline-contacts/" className="form-control nav-link">Airline Contacts</a>
-                    <a href="/mongo-office/association-contacts/" className="form-control nav-link">Association Contacts</a>
-                    <a href="/mongo-office/embassy-contacts/" className="form-control nav-link">Embassy Contacts</a>
-                    <a href="/mongo-office/visa-centers/" className="form-control nav-link">Visa Centers</a>
+                    <Link to="/mongo-office/task-manager/" className="form-control nav-link">Task Manager</Link>
+                    <Link to="/mongo-office/event-manager/" className="form-control nav-link">Event Manager</Link>                    
+                    <Link to="/mongo-office/file-tracker/" className="form-control nav-link">File Tracker</Link>
+                    <Link to="/mongo-office/duty-roster/" className="form-control nav-link">Duty Roster</Link>
+                    <Link to="/mongo-office/cash-register/" className="form-control nav-link">Cash Register</Link>                   
+                    <Link to="/mongo-office/vehicle-records/" className="form-control nav-link">Vehicle Records</Link> 
+                    <Link to="/mongo-office/travel-records/" className="form-control nav-link">Travel Records</Link>
+                    <Link to="/mongo-office/meeting-records/" className="form-control nav-link">Meeting Records</Link>
+                    <Link to="/mongo-office/medical-records/" className="form-control nav-link">Medical Records</Link>                    
+                    <Link to="/mongo-office/plan-master/" className="form-control nav-link">Plan Master</Link>
+                    <Link to="/mongo-office/project-manager/" className="form-control nav-link">Project Manager</Link>        
+                    <Link to="/mongo-office/business-contacts/" className="form-control nav-link">Business Contacts</Link>
+                    <Link to="/mongo-office/employee-contacts/" className="form-control nav-link">Employee Contacts</Link>
+                    <Link to="/mongo-office/airline-contacts/" className="form-control nav-link">Airline Contacts</Link>
+                    <Link to="/mongo-office/association-contacts/" className="form-control nav-link">Association Contacts</Link>
+                    <Link to="/mongo-office/embassy-contacts/" className="form-control nav-link">Embassy Contacts</Link>
+                    <Link to="/mongo-office/visa-centers/" className="form-control nav-link">Visa Centers</Link>
                 </div>
             </div>
         </div>
