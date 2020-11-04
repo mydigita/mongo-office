2c9bd8cc09bdd83cc40da203f94694eebef59360
thetradecoder <thetradecoder@gmail.com>
2020-11-04 09:54:22 +0600

    local update

--- src/components/side-nav.component.js
+++ src/components/side-nav.component.js
@@ -19,6 +19,7 @@ export default function SideNav(){
                     <a href="/business-contacts/" className="form-control nav-link">Business Contacts</a>
                     <a href="/employee-contacts/" className="form-control nav-link">Employee Contacts</a>
                     <a href="/airline-contacts/" className="form-control nav-link">Airline Contacts</a>
+                    <a href="/association-contacts/" className="form-control nav-link">Association Contacts</a>
                     <a href="/embassy-contacts/" className="form-control nav-link">Embassy Contacts</a>
                     <a href="/file-tracker/" className="form-control nav-link">File Tracker</a>
                     <a href="/travel-records/" className="form-control nav-link">Travel Records</a>
@@ -27,7 +28,7 @@ export default function SideNav(){
                     <a href="/plan-master/" className="form-control nav-link">Plan Master</a>
                     <a href="/project-manager/" className="form-control nav-link">Project Manager</a>
                     <a href="/vehicle-records/" className="form-control nav-link">Vehicle Records</a>            
-                    <a href="/driver-duty-roster/" className="form-control nav-link">Driver's Duty Roster</a>
+                    <a href="/duty-roster/" className="form-control nav-link">Duty Roster</a>
                 </div>
             </div>
         </div>
