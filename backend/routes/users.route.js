549cac43c5621af6a5cea3e5c960ae16e23ef223
thetradecoder <thetradecoder@gmail.com>
2020-11-05 12:16:10 +0600

    local update

---
+++ backend/routes/users.route.js
@@ -0,0 +1,21 @@
+const router = require('express').Router();
+const User = require('../models/user.model.js');
+
+router.route('/add')
+.post((req, res)=>{
+    const {
+        username,
+        user_email,
+        password,
+        full_name,
+    } = req.body;
+
+    var first_ip;
+    var last_ip;
+    var ip;
+    const created_on = new Date();
+    const updated_on = new Date();
+    var update_count;
+    var mac;
+    var browser;
+})
