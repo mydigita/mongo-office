0d595750f49f8f6a9e47c79059e2f6ee922827a5
thetradecoder <thetradecoder@gmail.com>
2020-11-05 11:54:25 +0600

    user model created with mongoose model

---
+++ backend/models/user.model.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+    username:{type:String},
+    user_email:{type:String},
+    password:{type:String},
+    full_name:{type:String},
+    first_ip:{type:String},
+    last_ip:{type:String},
+    created_on:{type:Date},
+    updated_on:{type:Date},
+    update_count:{type:Number},
+    history:[{
+        full_name:{type:String},
+        password:{type:String},
+        ip:{type:String},
+        updated_on:{type:Date}
+    }]
+}, {timestamps:true});
+
+const User = mongoose.model("User", UserSchema);
+
+module.exports = User;
