9133dc2a6a765f8bf16e1a75b74bea1df470ff98
thetradecoder <thetradecoder@gmail.com>
2020-11-06 23:12:53 +0600

    local update

--- src/components/users.component.js
+++ src/components/users.component.js
@@ -1,2 +1,34 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.js';
+import 'jquery/dist/jquery.js';
+
+export default function UserForm(){
+    return(
+        <div>
+            <form>
+                <div className="form-group">
+                    <label>Username:</label>
+                    <input type="text" className="form-control" placeholder="username"/>
+                </div>
+                <div className="form-group">
+                    <label>Email:</label>
+                    <input type="email" className="form-control" placeholder="email"/>
+                </div>
+                <div className="form-group">
+                    <label>Password:</label>
+                    <input type="password" className="form-control" placeholder="password"/>
+                </div>
+                <div className="form-group">
+                    <label>Full Name:</label>
+                    <input type="text" className="form-control" placeholder="full name"/>
+                </div>
+                <div>
+                    <button type="submit">Signup</button>
+                </div>
+                
+
+            </form>
+        </div>
+    );
+}
