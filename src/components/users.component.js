f3f353299ce3d05160b9c3a944e949aa890e3f89
thetradecoder <thetradecoder@gmail.com>
2020-11-08 13:11:09 +0600

    local update

--- src/components/users.component.js
+++ src/components/users.component.js
@@ -32,7 +32,7 @@ export default function UserForm(){
     function onSubmitUserSignup(e){
         e.preventDefault();
         axios.post('http://localhost:5000/mongo-office/accounts/register', userDetails)
-        .then(()=>window.alert("Signup Successful"))
+        .then(()=>{window.alert("Signup Successful"); window.location='/mongo-office/home'})
         .catch(err=>window.alert(err));
     }
     return(
