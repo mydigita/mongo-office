4265140a2f52c85051d198944ed70efc0e368c3c
thetradecoder <thetradecoder@gmail.com>
2020-11-09 20:57:19 +0600

    local update

--- src/components/login.component.js
+++ src/components/login.component.js
@@ -1,13 +1,37 @@
-import React from 'react';
+import React, {useState} from 'react';
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.js";
 import "jquery/dist/jquery.js";
 
 export default function UserLogin(){
+    const [username, setUsername]=useState("");
+    const [password, setPassword]=useState("");
+
+    function onChangeUsername(e){
+        setUsername(e.target.value);
+    }
+    function onChangePassword(e){
+        setPassword(e.target.value);
+    }
+
+
+    function onSubmitUserLogin(e){
+        e.preventDefault();
+        
+    }
+
     return(
         <div>
-            <form>
-                
+            <form onSubmit={onSubmitUserLogin}>
+                <div className="form-group">
+                    <label>Username</label>
+                    <input type="text" className="form-control" onChange={onChangeUsername} placeholder="username" requireed/>
+                </div>
+                <div className="form-group">
+                    <label>Password</label>
+                    <input type="password" className="form-control" onChange={onChangePassword} placeholder="password" required/>
+                </div>
+
             </form>
 
         </div>
