ad065569e731e29a972c5d15af26301fff067b27
thetradecoder <thetradecoder@gmail.com>
2020-11-09 20:48:45 +0600

    working on login component

---
+++ src/components/login.component.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.js";
+import "jquery/dist/jquery.js";
+
+export default function UserLogin(){
+    return(
+        <div>
+            <form>
+                
+            </form>
+
+        </div>
+    );
+}
