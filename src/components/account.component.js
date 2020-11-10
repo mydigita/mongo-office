411547aa1ccc36a786dfc1b6635117504311374e
thetradecoder <thetradecoder@gmail.com>
2020-11-10 16:17:47 +0600

    local update

---
+++ src/components/account.component.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.js';
+import 'jquery/dist/jquery.js';
+
+export default function Account(){
+    return(
+        <div>
+            
+        </div>
+    );
+}
