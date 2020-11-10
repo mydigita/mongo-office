2ca3811737f748a071afb1aec398007d322b2523
thetradecoder <thetradecoder@gmail.com>
2020-11-10 16:20:44 +0600

    local update

--- src/components/account.component.js
+++ src/components/account.component.js
@@ -1,12 +1,13 @@
 import React from 'react';
+import {BroserRouter as Router, Route} from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.js';
 import 'jquery/dist/jquery.js';
 
 export default function Account(){
     return(
-        <div>
-            
-        </div>
+        <Router>
+
+        </Router>
     );
 }
