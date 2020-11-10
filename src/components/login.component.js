b584503789536f4583b9ac014f4cc98b5a8a2f02
thetradecoder <thetradecoder@gmail.com>
2020-11-10 16:14:49 +0600

    local update

--- src/components/login.component.js
+++ src/components/login.component.js
@@ -19,6 +19,9 @@ export default function UserLogin(){
     function onSubmitUserLogin(e){
         e.preventDefault();
         const userData = {username, password};
+        axios.get(`http://localhost:5000/${username}/${password}`)
+        .then(()=>window.location="/mongo-office/home")
+        .catch(err=>window.alert(err))
                 
     }
 
