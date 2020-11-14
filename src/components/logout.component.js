d5f064c4d490a81b5be6f502e4f2b0db014857cf
thetradecoder <thetradecoder@gmail.com>
2020-11-14 17:14:43 +0600

    local update

--- src/components/logout.component.js
+++ src/components/logout.component.js
@@ -12,22 +12,20 @@ export default function Logout(){
         window.location = '/mongo-office/';
     }
     function renderLogout(){
-        if(user.length){
+        if(user){
             return(
                 <div className="body-part">
-            <form onSubmit={onSubmitLogoutTrue}>
-                <h2>Hey {user} !</h2>
-                <p className="text-danger">Are you sure to logout?</p>
-                <button type="submit" className="btn btn-warning">Yes</button>            
-            </form>
-        </div>
-
-
+                    <form onSubmit={onSubmitLogoutTrue}>
+                        <h2>Hey {user} !</h2>
+                        <p className="text-danger">Are you sure to logout?</p>
+                        <button type="submit" className="btn btn-warning">Yes</button>            
+                    </form>
+                </div>
             );
         } else {
             return (
                 <div className="body-part">
-                    You are not logged in!
+                    <p className="text-danger">You are not logged in!</p>
                 </div>
             );
         }
