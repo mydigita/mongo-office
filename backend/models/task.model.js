54258c1b4ed56d1c854e93413d95cac5b31084ba
thetradecoder <thetradecoder@gmail.com>
2020-11-16 07:31:37 +0600

    local update

--- backend/models/task.model.js
+++ backend/models/task.model.js
@@ -2,31 +2,12 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const TaskSchema = new Schema({
-    username:{type:String},
     title:{type:String},
     details:{type:String},
-    assigned_to:{type:String},
-    created_by:{type:String},
-    date_created:{type:Date},
+    assignedTo:{type:String},
     deadline:{type:Date},
-    status:{type:String},
-    update_count:{type:Number},
-    open:{type:String},
-    closed:{type:String},     
-    deleted:{type:String},
-    visit_count:{type:Number},
-    history:[{
-        username:{type:String},
-        title:{type:String},
-        details:{type:String},
-        assigned_to:{type:String},
-        deadline:{type:Date},
-        status:{type:String},
-        ip:{type:String},
-        updated_on:{type:Date},
-        mac:{type:String},
-        browser:{type:String}
-    }]
+    progress:{type:String},
+    status:{type:String},    
 }, {timestamps:true});
 
 const Task = mongoose.model('Task', TaskSchema);
