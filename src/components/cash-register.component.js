5a7db62a49d1269744493826a496e32db4dad407
thetradecoder <thetradecoder@gmail.com>
2020-11-23 11:53:53 +0600

    local update

--- src/components/cash-register.component.js
+++ src/components/cash-register.component.js
@@ -7,70 +7,84 @@ import "../App.css";
 import DatePicker from 'react-datepicker';
 import "react-datepicker/dist/react-datepicker.css";
 
-// Keep record / data entry
-function RecordTransactions(){
-
-
-    return (
-        <div>
-            <form>
-                <div>
-                    <p>Date:</p>
-                </div>
-                <div className="form-group">
-                    <label>Description: </label>
-                    <input type="text" className="form-control"/>
-                </div>
-
-            </form>
-        </div>
-
-    );
-}
-
-export default function CashRegister(){
-    const [transactions, setTransactions] =  useState("");
-    const [balance, setBalance] = useState("");
-    const [cashIn, setCashIn] = useState("");    
-    const [cashOut, setCashOut] = useState("");
-    const [date] = useState(new Date());    
-    const [details, setDetails] = useState("");
-    const username = localStorage.getItem('username');
-    const userid = localStorage.getItem('userid');
-
+const username = localStorage.getItem('username');
+const userid = localStorage.getItem('userid');
 
 
 
 // View transactions / statement
-
-    function viewTransaction(){
-        axios.get(`http://localhost:5000/mongo-office/cash-register/view/${username}/${userid}`)
-        .then(data=>{
-            setTransactions(data.data.reverse().map(e=>{
-                return (
-                    <tr>
-                        <td>{e.date}</td>
-                        <td>{e.details}</td>
-                        <td>{e.cashIn}</td>
-                        <td>{e.cashOut}</td>
-                        <td>{e.balance}</td>
-                    </tr>
-                );
-            }))
-        })
+export default function CashRegister(){
+    const [transactions, setTransactions] =  useState("");
+    const date = new Date().toLocaleDateString();
+    const [details, setDetails]= useState("");
+    const [cashIn, setCashIn] =  useState(0);
+    const [cashOut, setCashOut] = useState(0);
+    function onChangeDetails(e){
+        setDetails(e.target.value);
     }
+    function onChangeCashIn(e){
+        if(e>0){
+            setCashIn(e.target.value)
+        }
+    }
+    function onChangeCashOut(e){
+        if(e>0){
+            setCashOut(e.target.value);
+        }
+    }
+
+    function onSubmitRecordTrx(e){
+        e.preventDefault();
+    }
+
+    // useEffect(()=>{
+
+    //     axios.get(`http://localhost:5000/mongo-office/cash-register/view/${username}/${userid}`)
+    //     .then(data=>{
+    //         setTransactions(data.data.reverse().map(e=>{
+    //             return (
+    //                 <tr>
+    //                     <td>{e.date}</td>
+    //                     <td>{e.details}</td>
+    //                     <td>{e.cashIn}</td>
+    //                     <td>{e.cashOut}</td>
+    //                     <td>{e.balance}</td>
+    //                 </tr>
+    //             );
+    //         }))
+    //     })
+
+    // }, []);
+
+
 
 
 
     return(
         <div className="body-part">
-            <div id="view">
+            {/* <div id="view">
                 <table>
                     {transactions}
                 </table>
-            </div>
+            </div> */}
             <div id="record">
-               <RecordTransactions/>
+                <form onSubmit={onSubmitRecordTrx}>
+                    <p className="btn btn-warning disabled">Date: {date}</p>
+                    <div className="form-group">
+                        <label>Description: </label>
+                        <input type="text" placeholder="Trx details" className="form-control" onChange={onChangeDetails} required/>
+                    </div>
+                    <div className="form-group">
+                        <label>Cash Received: </label>
+                        <input type="text" placeholder="Cash received" className="form-control" onChange={onChangeCashIn} required/>
+                    </div>
+                    <div className="form-group">
+                        <label>Expense Amount: </label>
+                        <input type="text" placeholder="Expense amount" className="form-control" onChange={onChangeCashOut} required/>
+                    </div>
+                    <button type="submit" className="btn btn-primary">Submit</button>
+                </form>
+               
             </div>
 
         </div>
