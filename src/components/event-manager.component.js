255247958a4323a0c5a5be5307542d639237d24f
thetradecoder <thetradecoder@gmail.com>
2020-11-27 15:51:21 +0600

    working on event manager

---
+++ src/components/event-manager.component.js
@@ -0,0 +1,16 @@
+import React, {useEffect, useState}  from 'react';
+import axios from 'axios';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.js";
+import "jquery/dist/jquery.js";
+import "../App.css";
+import DatePicker from 'react-datepicker';
+import "react-datepicker/dist/react-datepicker.css";
+
+export default function EventManager(){
+    return(
+        <div>
+            
+        </div>
+    );
+}
