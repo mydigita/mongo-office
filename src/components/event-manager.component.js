4fecc73f607b8cb4bdbf3b85f7304a7335d295a3
thetradecoder <thetradecoder@gmail.com>
2020-11-27 15:57:19 +0600

    local update

--- src/components/event-manager.component.js
+++ src/components/event-manager.component.js
@@ -8,9 +8,20 @@ import DatePicker from 'react-datepicker';
 import "react-datepicker/dist/react-datepicker.css";
 
 export default function EventManager(){
+    const [title, setTitle] = useState("");
+    const [details, setDetails] = useState("");
+    const [eventDate, setEventDate]= useState(new Date());
+    const [eventTime, setEventTime]= useState(new Date());
+    const [organizer, setOrganizer] = useState("");
+    const [venue, setVenue] = useState("");
+    const [contactPerson, setContactPerson] = useState("");    
+    const status = 'open';
+    const username = localStorage.getItem('user');
+    const userid= localStorage.getItem('userid');
     return(
         <div>
             
+
         </div>
     );
 }
