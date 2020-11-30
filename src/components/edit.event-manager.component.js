f238f6ad5df7ae76ba4ed106bd87cb61e4e51c26
thetradecoder <thetradecoder@gmail.com>
2020-11-30 23:01:47 +0600

    local update

---
+++ src/components/edit.event-manager.component.js
@@ -0,0 +1,21 @@
+import React, {useEffect, useState}  from 'react';
+import axios from 'axios';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.js";
+import "jquery/dist/jquery.js";
+import "../App.css";
+
+import DateFnsUtils from '@date-io/date-fns'; 
+import {  
+  DateTimePicker,
+  MuiPickersUtilsProvider,
+} from '@material-ui/pickers';
+import "react-datepicker/dist/react-datepicker.css";
+
+export default function EditEvent(){
+    return(
+        <div>
+            
+        </div>
+    );
+}
