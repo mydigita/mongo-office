30d07846662e26d0b7ba004c01daa852c47f1058
thetradecoder <thetradecoder@gmail.com>
2020-11-30 23:04:02 +0600

    local update

--- src/components/edit.event-manager.component.js
+++ src/components/edit.event-manager.component.js
@@ -13,9 +13,18 @@ import {
 import "react-datepicker/dist/react-datepicker.css";
 
 export default function EditEvent(){
+    const [title, setTitle] = useState("");
+    const [details, setDetails] = useState("");    
+    const [organizer, setOrganizer] = useState("");
+    const [venue, setVenue] = useState("");
+    const [contactDetails, setContactDetails] = useState("");    
+    const username = localStorage.getItem('user');
+    const userid= localStorage.getItem('userid'); 
+    const [eventDate, setEventDate] = useState(new Date());
+  
     return(
         <div>
-            
+
         </div>
     );
 }
