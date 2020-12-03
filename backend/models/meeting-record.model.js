fd5e9eb0941e1054ff262b50fa3d65ee3dd40e5d
thetradecoder <thetradecoder@gmail.com>
2020-12-03 21:07:21 +0600

    completed meeting-record data model

--- backend/models/meeting-record.model.js
+++ backend/models/meeting-record.model.js
@@ -10,9 +10,11 @@ const meetingSchema = new Schema({
     venue:{type:String},
     notice:{type:String},
     noticeDistribution:[String],
-    chair:{type:String},
+    chairedBy:{type:String},
     participants:[String], 
     minutes:{type:String},
+    minutesPreparedBy:{type:String},
+    minutesApprovedBy:{type:String},
     minutesDistribution:[String],
     username:{type:String},
     userid:{type:String}
