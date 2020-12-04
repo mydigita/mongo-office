703fbbf0b0cf7399b0180b4dc1cbaffebd658983
thetradecoder <thetradecoder@gmail.com>
2020-12-04 19:52:38 +0600

    completed meeting-record backend

--- backend/routes/meeting-record.route.js
+++ backend/routes/meeting-record.route.js
@@ -1,6 +1,22 @@
 const router = require("express").Router();
 const MeetingRecord = require("../models/meeting-record.model");
 
+router.route('/view-list/:username/:userid')
+.get((req,res)=>{
+    const {username, userid} = req.params;
+    MeetingRecord.find({_id:userid, username}, {_id:true, meetingId:true, title:true, status:true})
+    .then(data=>res.send(data))
+    .catch(err=>res.send(err))
+});
+
+router.route('/view-single/:id')
+.get((req, res)=>{
+    const {id} = req.params;
+    MeetingRecord.find({_id:id})
+    .then(data=>res.send(data))
+    .catch(err=>res.send(err))
+});
+
 
 router.route('/register/:username/:userid')
 .post((req, res)=>{
@@ -29,6 +45,7 @@ router.route('/register/:username/:userid')
         minutesPreparedBy:"",
         minutesApprovedBy:"",
         minutesDistribuion:[],
+        status:"open",
         username,
         userid
     });
@@ -39,7 +56,7 @@ router.route('/register/:username/:userid')
 });
 
 router.route('/add-minutes/:id')
-.post((req, res)=>{
+.put((req, res)=>{
     const {id}=req.params;
     const {minutes, minutesPreparedBy, minutesApprovedBy} = req.ready;
     MeetingRecord.findOneAndUpdate({_id:id}, {$set:{minutes, minutesPreparedBy, minutesApprovedBy}})
@@ -48,12 +65,20 @@ router.route('/add-minutes/:id')
 });
 
 router.route('/add-participants/:id')
-.post((req,res)=>{
+.put((req,res)=>{
     const {id} = req.params;
     const {participants, chairedBy} = req.body;
     MeetingRecord.findOneAndUpdate({_id:id}, {$push:{participants}, $set:{chairedBy}})
     .then(()=>res.send('Participants added'))
     .catch(err=>res.send(err))
-})
+});
+
+router.rotue('/edit/close/:id')
+.put((req, res)=>{
+    const {id}= req.params;
+    MeetingRecord.findOneAndUpdate({_id:id}, {$set:{status:"closed"}})
+    .then(()=>res.send("Meeting is closed!"))
+    .catch(err=>res.send(err))
+});
 
 module.exports = router;
