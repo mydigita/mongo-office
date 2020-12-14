5d9d757b90cc1fc6aabbcab254a3172b698684da
thetradecoder <thetradecoder@gmail.com>
2020-12-14 15:32:15 +0600

    update vehicle-records shcema - model

--- backend/models/vehicle-records.model.js
+++ backend/models/vehicle-records.model.js
@@ -3,11 +3,51 @@ const Schema =  mongoose.Schema;
 
 const VehicleSchema =  new Schema({
     carNumber:{type:String},
-    driverName:{type:String},
-    carDetails:[],
-    movementDetails:{type:String},    
-    repairHistory:[]
-});
+    carDetails:[{
+        color:{type:String},
+        taxTokenValidity:{type:Date},
+        routePermitValidity:{type:Date},
+        insuranceValidity:{type:Date}
+    }],
+    lastMovement:{
+        tripDate:{type:Date},
+        moveFrom:{type:String},
+        moveTo:{type:String},
+        driver:{type:String},
+        orderBy:{type:String},
+        tripDetails:{type:String}
+    },
+    movementHistory:[{
+        tripDate:{type:Date},
+        moveFrom:{type:String},
+        moveTo:{type:String},
+        driver:{type:String},
+        orderBy:{type:String},
+        tripDetails:{type:String}
+    }],    
+    repairHistory:[{
+        repairDate:{type:String},
+        problemDetails:{type:String},
+        repairBy:{type:String},
+        confirmBy:{type:String},
+        workDetails:{type:String},
+        cost:{type:Number}
+    }],
+    caseHistory:[{
+        driver:{type:String},
+        caseDate:{type:Date},
+        caseReason:{type:String},
+        caseStatus:{type:String},
+        caseDetails:{type:String}
+    }],
+    accidentHistory:[{
+        driver:{type:String},
+        accidentDate:{type:Date},
+        accidentDetails:{type:String}
+    }],
+    username:{type:String},
+    userid:{type:String}
+}, {timestamps:true});
 
 const VehicleRecords = mongoose.model("VehicleRecords", VehicleSchema);
 
